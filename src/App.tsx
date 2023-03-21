d119bd9f3b51d30f1f7e935bda3131ac589786e7
Anna Krasilnikova <akr6363@mail.ru>
2023-03-21 13:51:15 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useEffect} from 'react';
 import './App.css';
+import axios from "./axios";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    type PostType = {
+        _id: string,
+        title: string,
+        text: string,
+        createdAt: string,
+        updatedAt: string,
+        __v: number,
+
+    }
+
+
+    let [posts, setPosts] = useState<PostType[]>([])
+
+    useEffect(()=> {
+        axios.get('/posts').then(res => {
+            setPosts(res.data)
+        })
+    }, [])
+
+    let [newPostValue, setNewPostValue] = useState<string>('')
+
+
+    const onChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        setNewPostValue(event.currentTarget.value)
+    }
+
+    const addPostOnClickHandler = () => {
+        axios.post('/posts', {text: newPostValue}).then(res => {
+           setPosts([res.data, ...posts])
+        })
+        setNewPostValue('')
+    }
+
+    const deletePost = (id: string) => {
+        axios.delete(`/posts/${id}`).then(res => {
+            setPosts(posts.filter(post => post._id !== id))
+        })
+    }
+
+    return (
+        <div className="App">
+            <textarea value={newPostValue} onChange={onChangeHandler}></textarea>
+            <button onClick={addPostOnClickHandler}> add new post</button>
+            {posts.map((post) => {
+
+                const deletePostHandler = () => {
+                    deletePost(post._id)
+                }
+
+                return (
+                    <div>
+                        <p>{post.text}</p>
+                        <button onClick={deletePostHandler}>X</button>
+                    </div>
+                )
+            })}
+        </div>
+    );
 }
 
 export default App;
